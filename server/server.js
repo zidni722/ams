9ea9d68d9e1ec45ce0e7c44019ea965fa1aa7dd9
Zidni ilman <zidni722@gmail.com>
2020-09-01 00:54:21 +0700

    update.

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const buildPath = path.join(__dirname, '..', 'build');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(buildPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log('Server is up!');
+});
